47fcdc6d6509dd0b5e6ee3c665ca182463816a4c
THWU0412 <thimo.wuttge@hotmail.com>
2025-08-18 08:48:45 +0000

    Add data and none granularity test

--- src/granularity.py
+++ src/granularity.py
@@ -66,12 +66,25 @@ def test_rapl_granularity(granularity):
     logger.info(f"Executed granularity test for RAPL with granularity: {granularity}, errors: {error_counter}, duration: {duration}")
     return duration
 
+def test_none_granularity(granularity):
+    now = datetime.now()
+
+    measurements_counter = 0
+    
+    while measurements_counter < 100:
+        measurements_counter += 1
+        sleep(granularity)
+    duration = (datetime.now() - now).total_seconds()
+    print(f"Executed granularity test for NONE with granularity: {granularity}, duration: {duration}")
+    logger.info(f"Executed granularity test for NONE with granularity: {granularity}, duration: {duration}")
+    return duration
+
 def store_results(results_dir, granularity, durations_pdu, durations_redfish, durations_rapl):
     csv_file = os.path.join(results_dir, "granularity.csv")
     with open(csv_file, mode='a', newline='') as f:
         writer = csv.writer(f)
         if f.tell() == 0:
-            writer.writerow(["GRANULARITY", "PDU_DURATION", "Redfish_DURATION", "RAPL_DURATION"])
+            writer.writerow(["GRANULARITY", "NONE", "PDU_DURATION", "Redfish_DURATION", "RAPL_DURATION"])
         writer.writerow([granularity, np.mean(durations_pdu), np.mean(durations_redfish), np.mean(durations_rapl)])
 
 def run_granularity_tests(results_dir):
@@ -79,11 +92,13 @@ def run_granularity_tests(results_dir):
         durations_pdu = []
         durations_redfish = []
         durations_rapl = []
+        durations_none = []
         for i in range(2):
             durations_pdu.append(test_pdu_granularity(granularity))
             durations_redfish.append(test_redfish_granularity(granularity))
             durations_rapl.append(test_rapl_granularity(granularity))
-        store_results(results_dir, granularity, durations_pdu, durations_redfish, durations_rapl)
+            durations_none.append(test_none_granularity(granularity))
+        store_results(results_dir, granularity, durations_pdu, durations_redfish, durations_rapl, durations_none)
         print(f"Results for granularity {granularity}:")
         logger.info(f"Results for granularity {granularity}:")
         print(f"PDU durations: {np.mean(durations_pdu)}")
